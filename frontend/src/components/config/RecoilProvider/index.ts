d7df1012dd9ff3363cdddda4f01b15ecc435bcdc
MINSEON\minseon <duiph558291@gmail.com>
2026-04-03 17:49:38 +0900

    refactor: RecoilProviderMain 내보내기 방식 변경 및 타입 추가

---
+++ frontend/src/components/config/RecoilProvider/index.ts
@@ -0,0 +1,7 @@
+import RecoilProviderMain, { RecoilProviderMainProps } from "@/components/config/RecoilProvider/Main"
+
+export type { RecoilProviderMainProps }
+
+export default Object.assign(RecoilProviderMain, {
+  //
+})
