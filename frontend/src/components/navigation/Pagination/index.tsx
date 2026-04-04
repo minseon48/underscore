02179bd1d9c8d5d40cf4d16bd3b1bea3107a3b04
MINSEON\minseon <duiph558291@gmail.com>
2026-04-04 14:12:45 +0900

    chore : Pagination export 정리

---
+++ frontend/src/components/navigation/Pagination/index.tsx
@@ -0,0 +1,7 @@
+import PaginationMain, { PaginationMainProps } from "@/components/navigation/Pagination/Main"
+
+export type { PaginationMainProps }
+
+export default Object.assign(PaginationMain, {
+  //
+})
