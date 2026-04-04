1841de18634e248a75cf6fb9ef8b329c8f7bb9fa
MINSEON\minseon <duiph558291@gmail.com>
2026-04-04 12:46:44 +0900

    chore : Button export 정리

---
+++ frontend/src/components/general/Button/index.ts
@@ -0,0 +1,9 @@
+import ButtonMain, { ButtonMainProps, ButtonMainComponent } from "@/components/general/Button/Main"
+import { ButtonShape, ButtonSize, ButtonVariants } from "@/components/general/Button/type"
+
+export { ButtonShape, ButtonSize, ButtonVariants }
+export type { ButtonMainProps, ButtonMainComponent }
+
+export default Object.assign(ButtonMain, {
+  //
+})
