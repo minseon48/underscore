7725c0cc584cc100ba2bcb31923173558894a69d
MINSEON\minseon <duiph558291@gmail.com>
2026-04-04 15:14:14 +0900

    feat: MypagePaymentDetailMain 컴포넌트 진입점 및 확장 구조 설정

---
+++ frontend/src/components/page/MypagePaymentDetail/index.ts
@@ -0,0 +1,7 @@
+import MypagePaymentDetailMain, { MypagePaymentDetailMainProps } from "@/components/page/MypagePaymentDetail/Main"
+
+export type { MypagePaymentDetailMainProps }
+
+export default Object.assign(MypagePaymentDetailMain, {
+  //
+})
