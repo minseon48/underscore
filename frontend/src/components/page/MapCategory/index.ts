f95fb422d5559f553be0d601a6f92f204c70ed0b
MINSEON\minseon <duiph558291@gmail.com>
2026-04-04 15:09:07 +0900

    feat: MapCategoryMain 컴포넌트 진입점 및 확장 구조 설정

---
+++ frontend/src/components/page/MapCategory/index.ts
@@ -0,0 +1,7 @@
+import MapCategoryMain, { MapCategoryMainProps } from "@/components/page/MapCategory/Main"
+
+export type { MapCategoryMainProps }
+
+export default Object.assign(MapCategoryMain, {
+  //
+})
