4a79ecddc3d25959375a29d30e13f4325ea7cea8
MINSEON\minseon <duiph558291@gmail.com>
2026-04-04 14:06:19 +0900

    chore : Copyright export 정리

---
+++ frontend/src/components/navigation/Copyright/index.ts
@@ -0,0 +1,7 @@
+import CopyrightMain, { CopyrightMainProps } from "@/components/navigation/Copyright/Main"
+
+export type { CopyrightMainProps }
+
+export default Object.assign(CopyrightMain, {
+  //
+})
