6ec5c1e6e2467a5aa7aac9125313fd5ed5b9286d
MINSEON\minseon <duiph558291@gmail.com>
2026-04-04 15:13:40 +0900

    feat: MypageLeaveMembershipMain 컴포넌트 진입점 및 확장 구조 설정

---
+++ frontend/src/components/page/MypageLeaveMembership/index.ts
@@ -0,0 +1,9 @@
+import MypageLeaveMembershipMain, {
+  MypageLeaveMembershipMainProps,
+} from "@/components/page/MypageLeaveMembership/Main"
+
+export type { MypageLeaveMembershipMainProps }
+
+export default Object.assign(MypageLeaveMembershipMain, {
+  //
+})
