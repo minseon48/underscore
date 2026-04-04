393bac2ec5b60450d25414ebc076e7a7a2ab79a4
MINSEON\minseon <duiph558291@gmail.com>
2026-04-04 15:11:18 +0900

    feat: MapMyplaceMain 컴포넌트 진입점 및 확장 구조 설정

---
+++ frontend/src/components/page/MayMyplace/index.ts
@@ -0,0 +1,7 @@
+import MapMyplaceMain, { MapMyplaceMainProps } from "@/components/page/MapMyplace/Main"
+
+export type { MapMyplaceMainProps }
+
+export default Object.assign(MapMyplaceMain, {
+  //
+})
