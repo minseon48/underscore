2fb833076adb344fd4e595dafd543d1b88b5c2fe
MINSEON\minseon <duiph558291@gmail.com>
2026-04-04 12:47:48 +0900

    chore : Icon·TypeIconName export 및 default 구성

---
+++ frontend/src/components/general/Icon/index.tsx
@@ -0,0 +1,11 @@
+import * as data from "@/components/general/Icon/import"
+import { TypeIconName } from "@/components/general/Icon/type"
+import IconMain from "@/components/general/Icon/Main"
+
+export const Icon = data
+
+export { TypeIconName }
+
+export default Object.assign(IconMain, {
+  //
+})
