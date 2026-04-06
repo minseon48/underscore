75fe73e0bdf7c1eab4df4386e86b5779b82f1632
MINSEON\minseon <duiph558291@gmail.com>
2026-04-06 15:24:33 +0900

    fix: not-found 페이지 추가

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import "@/styles/error.css"
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Underscore</h1>
+      <h2>Not Found</h2>
+    </div>
+  )
+}
+
+export default NotFound
