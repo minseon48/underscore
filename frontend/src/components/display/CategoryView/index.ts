a094609984d5094d42dc2fb856a4e083d3d34d1d
MINSEON\minseon <duiph558291@gmail.com>
2026-04-06 15:26:22 +0900

    fix: Item 경로 대문자에서 소문자로 변경

--- frontend/src/components/display/CategoryView/index.ts
+++ frontend/src/components/display/CategoryView/index.ts
@@ -1,6 +1,6 @@
 import CategoryViewMain, { CategoryViewMainProps } from "@/components/display/CategoryView/Main"
 import CategoryViewGroup, { CategoryViewGroupProps } from "@/components/display/CategoryView/Group"
-import CategoryViewItem, { CategoryViewItemProps } from "@/components/display/CategoryView/Item"
+import CategoryViewItem, { CategoryViewItemProps } from "@/components/display/CategoryView/item.tsx"
 
 export type { CategoryViewMainProps, CategoryViewGroupProps, CategoryViewItemProps }
 
