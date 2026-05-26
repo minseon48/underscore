27b68ce74204906db4fcbded0fe6e0a102956c64
MINSEON\minseon <duiph558291@gmail.com>
2026-05-26 16:22:32 +0900

    feat : 유동인구 Repository 생성

---
+++ src/main/java/com/kodong/underscore/map/repository/FloatingPopulationRepository.java
@@ -0,0 +1,19 @@
+package com.kodong.underscore.map.repository;
+
+import com.kodong.underscore.map.entity.AdministrativeDistrict;
+import com.kodong.underscore.map.entity.FloatingPopulation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FloatingPopulationRepository extends JpaRepository<FloatingPopulation,Long> {
+    Optional<FloatingPopulation> findByStandardYearQuarterCodeAndAdministrativeDistrict(String standardYearQuarterCode, AdministrativeDistrict administrativeDistrictCode);
+    Page<FloatingPopulation> findByStandardYearQuarterCode(String standardYearQuarterCode, Pageable pageable);
+    List<FloatingPopulation> findAllByStandardYearQuarterCodeOrderByTotFloatingPopulationCoAsc(String standardYearQuarterCode);
+
+}
