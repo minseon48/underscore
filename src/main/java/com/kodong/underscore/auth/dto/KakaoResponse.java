e8972a5c68f85a6d080feae43f79be7c2948b0cb
MINSEON\minseon <duiph558291@gmail.com>
2026-06-14 17:22:22 +0900

    feat: Kakao OAuth2 사용자 정보 매핑용 KakaoResponse 추가

---
+++ src/main/java/com/kodong/underscore/auth/dto/KakaoResponse.java
@@ -0,0 +1,31 @@
+package com.kodong.underscore.auth.dto;
+
+import java.util.Map;
+
+public class KakaoResponse implements OAuth2Response{
+    private final Map<String, Object> attribute;
+
+    public KakaoResponse(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return "null";
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
