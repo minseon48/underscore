2ecf6eebd207dbc139fb44d42cb4192b6eb0602c
MINSEON\minseon <duiph558291@gmail.com>
2026-06-24 01:55:40 +0900

    feat : findByStandardYearQuarterCode 조회 메서드 추가

--- src/main/java/com/kodong/underscore/map/repository/IncomeConsumptionRepository.java
+++ src/main/java/com/kodong/underscore/map/repository/IncomeConsumptionRepository.java
@@ -2,6 +2,8 @@ package com.kodong.underscore.map.repository;
 
 import com.kodong.underscore.map.entity.AdministrativeDistrict;
 import com.kodong.underscore.map.entity.IncomeConsumption;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,7 @@ import java.util.Optional;
 @Repository
 public interface IncomeConsumptionRepository extends JpaRepository<IncomeConsumption,Long> {
     Optional<IncomeConsumption> findByStandardYearQuarterCodeAndAdministrativeDistrict(String standardYearQuarterCode, AdministrativeDistrict administrativeDistrict);
+    Page<IncomeConsumption> findByStandardYearQuarterCode(String standardYearQuarterCode, Pageable pageable);
     List<IncomeConsumption> findAllByStandardYearQuarterCodeOrderByFoodExpenditureAmountAsc(String standardYearQuarterCode);
 
 }
