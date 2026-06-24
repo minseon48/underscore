69a1e6d991e46b46e0214cfa2e6ab8e3561568c1
MINSEON\minseon <duiph558291@gmail.com>
2026-06-24 14:07:39 +0900

    feat: Builder Annotation 추가

--- src/main/java/com/kodong/underscore/auth/entity/User.java
+++ src/main/java/com/kodong/underscore/auth/entity/User.java
@@ -4,11 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
