4697e3d032acbdd89ebc2faaeee9fe94ca2d123c
MINSEON\minseon <duiph558291@gmail.com>
2026-09-02 20:47:23 +0900

    fix : 결제내역 페이지 prefetch 기간 필터 제거

--- frontend/src/components/form/SearchPayment/type.ts
+++ frontend/src/components/form/SearchPayment/type.ts
@@ -1,3 +1,5 @@
+import { FieldValues } from "react-hook-form"
+
 export const TypePaymentStateCode = {
   All: "All",
   PaymentScheduled: "PaymentScheduled",
@@ -8,23 +10,11 @@ export const TypePaymentStateCode = {
 
 export type TypePaymentStateCode = (typeof TypePaymentStateCode)[keyof typeof TypePaymentStateCode]
 
-export const TypePaymentPeriodCode = {
-  ["All"]: "All",
-  ["1M"]: "1M",
-  ["3M"]: "3M",
-  ["6M"]: "6M",
-  ["12M"]: "12M",
-  ["Custom"]: "custom",
-} as const
-
-export type TypePaymentPeriodCode = (typeof TypePaymentPeriodCode)[keyof typeof TypePaymentPeriodCode]
-
 export const PaymentStateOptionGroups = [
   {
     label: "결제상태",
     options: [
       { value: TypePaymentStateCode.All as TypePaymentStateCode, text: "전체" },
-      { value: TypePaymentStateCode.PaymentScheduled as TypePaymentStateCode, text: "결제예정" },
       { value: TypePaymentStateCode.PaymentCompleted as TypePaymentStateCode, text: "결제완료" },
       { value: TypePaymentStateCode.CancellationComplete as TypePaymentStateCode, text: "취소완료" },
       { value: TypePaymentStateCode.PaymentFailed as TypePaymentStateCode, text: "결제실패" },
@@ -32,16 +22,14 @@ export const PaymentStateOptionGroups = [
   },
 ]
 
-export const PaymentPeriodOptionGroups = [
-  {
-    label: "조회기간",
-    options: [
-      { value: TypePaymentPeriodCode["All"] as TypePaymentPeriodCode, text: "전체" },
-      { value: TypePaymentPeriodCode["1M"] as TypePaymentPeriodCode, text: "1개월" },
-      { value: TypePaymentPeriodCode["3M"] as TypePaymentPeriodCode, text: "3개월" },
-      { value: TypePaymentPeriodCode["6M"] as TypePaymentPeriodCode, text: "6개월" },
-      { value: TypePaymentPeriodCode["12M"] as TypePaymentPeriodCode, text: "12개월" },
-      { value: TypePaymentPeriodCode["Custom"] as TypePaymentPeriodCode, text: "직접입력" },
-    ],
-  },
-]
+export interface TypeSearchPayment extends FieldValues {
+  page: number
+  size: number
+  isFiltered: boolean
+  paymentStateCode: TypePaymentStateCode
+  startDate?: Date
+  endDate?: Date
+  searchPaymentStateCode: TypePaymentStateCode
+  searchStartDate?: Date
+  searchEndDate?: Date
+}
